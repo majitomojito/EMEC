e319e81a59ee739ce20b77260e0571fad34f3baa
osmaralg <osmar.alcala94@gmail.com>
2021-03-23 23:56:10 +0100

    query graph

--- articles/views.py
+++ articles/views.py
@@ -4,13 +4,10 @@ from django.http.response import JsonResponse
 from django.http import HttpResponse
 from django.views import generic
 from .models import *
-from tensorflow import keras
-from PIL import Image
-from io import BytesIO
-import base64
-import re
+
 import numpy as np
-import json 
+import json
+from .functions import simulate
 
 def index(request):
     articles = Article.objects.all()
@@ -35,29 +32,16 @@ def predict_number(request, *args, **kwargs):
     
     if request.method == 'POST':
         print(request)
-        canvas =  request.POST.get('canvas')
-        
-        canvas = re.sub('^data:image/.+;base64,', '', canvas)
-        #print("canvas", canvas)
-        im = Image.open(BytesIO(base64.b64decode(canvas)))
-        im = im.resize((28,28))
-        im = im.convert('L')
-        print("image size", im.size)
 
+        df = simulate()
+        df_dict = df.to_dict('index')
+        result = []
+        colors = []
+        for key in df_dict:
+            result.append(df_dict[key])
+            if df_dict[key]['Infectious']:
+                colors.append('red')
+            else:
+                colors.append('black')
 
-        container = np.zeros((6,28,28), dtype='float64')
-        im = np.asarray(im)
-
-        print(im)
-        img = Image.fromarray(im, 'L')
-
-        im = ((im / 255.0) - 1 ) * -1
-        im[im < 0] = 0
-        container[0] = im.copy()
-
-        new_model = keras.models.load_model('path_to_my_model.h5')
-        print(new_model)
-        prediction = new_model.predict(container)
-
-        print("prediction", prediction)
-        return JsonResponse({'data': str(np.argmax(prediction)), 'prediction': json.dumps(prediction[0].tolist())})
+        return JsonResponse({'data': result, 'colors': colors})
